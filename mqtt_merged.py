078cb8bc301544e3dcc9c47b39a32b4ceac9eb5c
Chloe Pi <cragzu@gmail.com>
2019-12-02 17:11:10 -0800

    Created new merged file

--- mqtt_merged.py
+++ mqtt_merged.py
@@ -1,7 +1,12 @@
+import paho.mqtt.publish as publish
 import paho.mqtt.client as mqtt
- 
-MQTT_SERVER = "192.168.43.106"
+
+
+MQTT_CHLOE = "192.168.43.106" # Chloe's IP - the host
+MQTT_KAYDEN = "192.168.43.12" # Kayden's IP
+MQTT_JACKY = "192.168.43.142" # Jacky's IP
 MQTT_PATH = "test_channel"
+
  
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -20,13 +25,23 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
  
-client.connect(MQTT_SERVER, 1883, 60)
+client.connect(MQTT_CHLOE, 1883, 60)
  
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
 client.loop_forever()
+
+
+publish.single(MQTT_PATH, "The server was opened successfully.", hostname=MQTT_CHLOE)
+
+while True:
+    msg = input()
+    publish.single(MQTT_PATH, msg, hostname=MQTT_CHLOE)
+    publish.single(MQTT_PATH, msg, hostname=MQTT_KAYDEN)
+    publish.single(MQTT_PATH, msg, hostname=MQTT_JACKY)
+
 
 
 
--- mqtt_publisher.py
+++ mqtt_publisher.py
@@ -3,7 +3,7 @@ import paho.mqtt.publish as publish
 MQTT_SERVER = "142.232.155.59" # IP of other pi
 MQTT_PATH = "test_channel"
  
-publish.single(MQTT_PATH, "Hello World!", hostname=MQTT_SERVER)
+publish.multiple(MQTT_PATH, "Hello World!", hostname=MQTT_SERVER)
 
 while True:
     msg = input()
