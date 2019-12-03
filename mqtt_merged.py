0866e1a1285f90f566ff0dd92945c32a9bb7cbbb
Cragzu <cragzu@gmail.com>
2019-12-03 09:03:36 -0800

    Added docstring to on_message and more comments

--- mqtt_merged.py
+++ mqtt_merged.py
@@ -11,16 +11,16 @@ MQTT_CURRENT = MQTT_KAYDEN  # The current user's IP
 
  
 # The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc: int):  # middle 2 params are unused, will it break if they are removed?
+def on_connect(client, userdata, flags, rc: int):  # todo: middle 2 params are unused, will it break if they're removed?
     """
     Print a confirmation message upon successful connection.
 
-    :param client:
+    :param client: the device running the script and connecting to the MQTT server
     :param userdata: unused? remove?
     :param flags: unused? remove?
     :param rc: int, the result code of the connection.
     :precondition: rc should be 0 if the connection was successful.
-    :return: none, uses print statement.
+    :return: none, uses print statement
     """
     print("Connected with result code "+str(rc))
  
@@ -29,14 +29,28 @@ def on_connect(client, userdata, flags, rc: int):  # middle 2 params are unused,
     client.subscribe(MQTT_PATH)
  
 # The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
+def on_message(client, userdata, msg: str):  # todo: userdata unused? remove?
+    """
+    Publish a message typed by the client to the other devices.
+
+    :param client: the device running the script and connecting to the MQTT server
+    :param userdata: unused? remove?
+    :param msg: string, the message to be sent
+    :return: none, uses print statement
+    """
+
+    # client._host is the IP of the client, this distinguishes who sent what message
+    # decoding the message with utf-8 helps with formatting and removes quotes/other characters
     print(str(client._host) + ": " + str(msg.payload.decode('utf-8')))
-    # more callbacks, etc
- 
+
+
+# Set up the client, connect them to the server, allow them to send messages
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
- 
+
+# connect to the current client IP with port 1883 and
+# 60 keepalive (the rate ping messages are sent; or max time in seconds allowed between broker communications)
 client.connect(MQTT_CURRENT, 1883, 60)
  
 # Blocking call that processes network traffic, dispatches callbacks and handles reconnecting.
