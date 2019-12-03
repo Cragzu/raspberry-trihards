2e5a619c2be93463ec4fc2122cfed006008a732d
Kayden <nedtheyak@magmablock.x10host.com>
2019-12-03 13:21:28 -0800

    Removed unused arguments, modified docstrings, modified message print behaviour

--- mqtt_merged.py
+++ mqtt_merged.py
@@ -12,16 +12,16 @@ MQTT_CURRENT = MQTT_KAYDEN  # The current user's IP
 
  
 # The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc: int):  # todo: middle 2 params are unused, will it break if they're removed?
+def on_connect(client, userdata, flags, rc: int):
     """
     Print a confirmation message upon successful connection.
 
     :param client: the device running the script and connecting to the MQTT server
-    :param userdata: unused? remove?
+    :param userdata: user data of any type, set when creating the client instance
     :param flags: unused? remove?
     :param rc: int, the result code of the connection.
     :precondition: rc should be 0 if the connection was successful.
-    :return: none, uses print statement
+    :postcondition: print connection result
     """
     print("Connected with result code "+str(rc))
  
@@ -30,29 +30,30 @@ def on_connect(client, userdata, flags, rc: int):  # todo: middle 2 params are u
     client.subscribe(MQTT_PATH)
  
 # The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg: str):  # todo: userdata unused? remove?
+def on_message(client, userdata, msg: str):
     """
     Publish a message typed by the client to the other devices.
 
     :param client: the device running the script and connecting to the MQTT server
-    :param userdata: unused? remove?
+    :param userdata: user data of any type, set when creating the client instance
     :param msg: string, the message to be sent
-    :return: none, uses print statement
+    :postcondition: print the message, preceded by the ip
     """
 
     # client._host is the IP of the client, this distinguishes who sent what message
     # decoding the message with utf-8 helps with formatting and removes quotes/other characters
-    print(str(msg.payload.decode('utf-8')))
+    print(str(userdata) + ": " + str(msg.payload.decode('utf-8')))
 
 
 # Set up the client, connect them to the server, allow them to send messages
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+client.user_data_set(MQTT_CURRENT)
 
 # connect to the current client IP with port 1883 and
 # 60 keepalive (the rate ping messages are sent; or max time in seconds allowed between broker communications)
-client.connect(MQTT_CURRENT, 1883, 60)
+client.connect(MQTT_CURRENT)
  
 # Blocking call that processes network traffic, dispatches callbacks and handles reconnecting.
 client.loop_start()
@@ -61,7 +62,7 @@ client.loop_start()
 publish.single(MQTT_PATH, "The server was opened successfully.", hostname=client._host)
 
 while True:  # continually ask for input to publish
-    msg = "Kayden: " + input()
+    msg = input()
     for i in [MQTT_CHLOE, MQTT_KAYDEN, MQTT_JACKY]:  # publish message to all 3 devices
         try:
             publish.single(MQTT_PATH, msg, hostname=i)
